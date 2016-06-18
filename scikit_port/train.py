fecbe7545cec2d6ead89a40579eff14cd817b1a6
Vishal Talwar <rawlatv@gmail.com>
2016-06-17 21:15:51 -0700

    Updating scikit port

--- scikit_port/train.py
+++ scikit_port/train.py
@@ -5,12 +5,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 
+from matlab_port.utils import partition_data, shuffle_data
 from train import load_data
 
 X, y = load_data('data/ex4data1_conv.mat', 'numpy')
+X, y = shuffle_data(X, y)
+X, y, X_test, y_test = partition_data(X, y, split=.8)
 
-classifier = LogisticRegression(C=1)
+classifier = LogisticRegression(C=.1)
 classifier.fit(X, y)
 
-z = classifier.predict(X)
-print(sum(y == z) / y.size)
+z = classifier.predict(X_test)
+print("Test accuracy: {acc:.1%}".format(acc=(sum(y_test == z) / y_test.size)))
